107e108567b1576c440fe44c5724659483b5e52c
FeliciaCalistaQK <161106015+FeliciaCalistaQK@users.noreply.github.com>
2024-10-31 22:38:37 +0700

    connect

--- mdp-acc/app_api/models/db.js
+++ mdp-acc/app_api/models/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(
-            "mongodb+srv://feliciacalista18:mongofeli88@cluster0.nffh2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb://feliciacalista18:123456feli@cluster0-shard-00-00.nffh2.mongodb.net:27017,cluster0-shard-00-01.nffh2.mongodb.net:27017,cluster0-shard-00-02.nffh2.mongodb.net:27017/?ssl=true&replicaSet=atlas-q2tt7s-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
         );
         console.log("mongoDB Connected...");
     } catch (error){
