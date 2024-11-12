4cfcb06a6a8fe1f591c514d588bd8fec7d24659d
FeliciaCalistaQK <161106015+FeliciaCalistaQK@users.noreply.github.com>
2024-11-12 10:21:48 +0700

    TES

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressLayout = require("express-ejs-layouts");
 const connectDB = require("./app_api/models/db");
 
-const cors = require('cors')
 var indexRouter = require('./app_server/routes/index');
+// var prodiRouter = require("./app_server/routes/prodi");
 var usersRouter = require('./app_server/routes/users');
-var prodiRouter = require('./app_server/routes/prodi');   
+const fakultasRouterApi = require("./app_api/routes/fakultas")
+
+// const prodiPageRouter = require('./app_server/routes/prodi');
+const fakultasRouter = require("./app_server/routes/fakultas");
+const prodiRouter = require("./app_server/routes/prodi");
+const prodiRouterApi = require("./app_api/routes/prodi");
+
 var app = express();
-const expressLayout = require("express-ejs-layouts"); // import modul express-ejs-layout
-const fakultasRouter = require("./app_api/routes/fakultas");
-const ProdiRouter = require("./app_api/routes/prodi");
+
 // view engine setup
-app.set('views', path.join(__dirname,'app_server','views'));
+app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors())
-app.use(logger('dev'));1
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -26,15 +30,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayout);
 
 app.use('/', indexRouter);
+app.use("/fakultas", fakultasRouter);
 app.use('/users', usersRouter);
-app.use('/prodi',prodiRouter);
-app.use("/api/fakultas", fakultasRouter);
-app.use("/api/prodi", ProdiRouter);
+app.use('/prodi', prodiRouter);
+//API
+app.use("/api/fakultas", fakultasRouterApi);
+app.use("/api/prodi", prodiRouterApi);
+
+connectDB();
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-connectDB();
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- app_server/controllers/fakultasController.js
+++ app_server/controllers/fakultasController.js
@@ -1,21 +1,24 @@
 const axios = require("axios");
-const index = async(req,res)=>{
+
+const index = async (req, res) => {
     try{
-        //mendapatkan data fal=kultas dari API eksternal
+
         const response = await axios.get(
             "https://crud-express-seven.vercel.app/api/fakultas"
         );
-        //Data fakultas dari API
         const fakultas = response.data;
-        //Render halaman 'fakultas dengan data yang diperoleh dari API
-        res.render("fakultas",{
+
+        res.render("fakultas", {
             title: "Halaman Fakultas",
             fakultas,
-            layout: 'main',
+            layout: "main",
         });
-    }catch(error){
-        //jika terjadi kesalahan saat mengambil data dari API
+
+    } catch (error) {
+
         console.error(error.message);
         res.status(500).send("Gagal mendapatkan data fakultas dari API");
     }
-}
+};
+
+module.exports = {index};
